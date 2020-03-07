cd64d2233f74cdb374aefe600bd848f97cda0242
Lucas Torti <torti@chapman.edu>
2020-03-07 15:04:12 +0000

    Added comments

--- Board.cpp
+++ Board.cpp
@@ -1,17 +1,28 @@
 //https://www.youtube.com/watch?v=fwYlzznrId4
 //https://www.cplusplus.com/reference/cstdlib/rand/
-//Assignment 1
+
+// 1) 
+// a. Lucas Torti
+// b. 2351555
+// c. torti@chapman.edu
+// d. CPSC350 - 01
+// e. Assignment 2, Game of Life
+// 2) 
+// This is a class which stores all the values for the board of game of life. In addition it contains helper functions.
+
+// 3) All classes/methods/functions/member variables/etc will be documented individually
+// with block comments providing the following:
 
 #include "Board.h"
 
-//default constructor
+//
 Board::Board()
 {
     height = 5;
 
     width = 5;
 
-//classic
+//sets up the 2d array
     boardArray = new bool*[height];
     for (int i = 0; i < height; ++i)
     {
@@ -21,18 +32,8 @@ Board::Board()
         }
     }
 
-//new
-    //     boardArray = new bool*[width];
-    // for (int i = 0; i < height; ++i)
-    // {
-    //     for (int j = 0; j < width; ++j)
-    //     {
-    //         boardArray[i] = new bool [height];
-    //     }
-    // }
 
-    //initialize array
-//classic
+    //initializes array to false
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
@@ -42,15 +43,6 @@ Board::Board()
         cout << endl;
     }
 
-//new
-    // for (int i = 0; i < width; ++i)
-    // {
-    //     for (int j = 0; j < height; ++j)
-    //     {
-    //         boardArray[j][i] = false;//random cells
-    //     }
-    //     cout << endl;
-    // }
 
 }
 
@@ -63,6 +55,8 @@ Board::Board(int xSizeInput, int ySizeInput)
 
     height = ySizeInput;
 
+
+    //sets up 2d array
     boardArray = new bool*[height];
     for (int i = 0; i < height; ++i)
     {
@@ -73,7 +67,7 @@ Board::Board(int xSizeInput, int ySizeInput)
         }
     }
 
-    //initialize array
+    //initialize array to false
 
     for (int j = 0; j < height; ++j)
     {
@@ -92,7 +86,10 @@ Board::~Board()
 
 }
 
-
+// a. printBoard
+// b. Returns no value
+// c. No parameter type
+// d. No exception types
 void Board::printBoard()
     {
         deblog("Starting outer loop");
@@ -136,6 +133,7 @@ void Board::printBoard()
 
     bool  Board::IsBoardEmpty()
 {
+    //iterates through array and returns false if any location in the array is full, otherwise returns true
      for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j <width; ++j)
@@ -155,6 +153,7 @@ string Board::GetBoardAsString()
 
     string boardValues = "";
 
+    //iterates through array, ceating a string of values as it does so
     for(int y = 0; y < height; ++y)
     {
         for(int x = 0; x < width; ++x)
@@ -171,6 +170,8 @@ string Board::GetBoardAsString()
         }
         boardValues += '\n';
     }
+
+    //provides some extra whitespace equal to the height of the board
     for(int y = 0; y < height; ++y)
     {
         boardValues += '\n';
